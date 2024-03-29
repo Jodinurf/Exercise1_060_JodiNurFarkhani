e3c5f624c078bbcef2b0e0e85b1a921c8551bb1d
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-03-29 19:42:37 +0700

    the javascript code for the script

---
+++ script.js
@@ -0,0 +1,7 @@
+function showAlert() {
+    alert("In 2010, a representative environment for studying has begun to be realized with lecture buildings, mosques and student dormitories.");
+  }
+
+function Paragraph1() {
+    alert("‘Lapangan Asri’ in Wirobrajan, Bantul, Yogyakarta was the first area to build UMY");
+}
